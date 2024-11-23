5d587ca6ff5fefa1588e6df5fdf98e2658d7cc78
Suryansh Naithani <95272710+Suryanshnaithani@users.noreply.github.com>
2024-11-23 21:33:33 +0530

    Update traffic_sign_model.py

--- traffic_sign_model.py
+++ traffic_sign_model.py
@@ -17,14 +17,8 @@ def import_and_predict(image_data, model):
         img_array = tf.expand_dims(img_array, 0)
         predictions = model.predict(img_array)
         score = tf.nn.softmax(predictions[0])
-        class_names=['Speed limit (20km/h)', 'Speed limit (30km/h)', 'Speed limit (50km/h)', 'Speed limit (60km/h)', 'Speed limit (70km/h)', 'Speed limit (80km/h)', 'End of speed limit (80km/h)', 'Speed limit (100km/h)', 'Speed limit (120km/h)', 'No passing', 'No passing for vehicles over 3.5 metric tons', 'Right-of-way at the next intersection', 'Priority road', 'Yield', 'Stop', 'No vehicles', 'Vehicles over 3.5 metric tons prohibited', 'No entry', 'General caution', 'Dangerous curve to the left', 'Dangerous curve to the right', 'Double curve', 'Bumpy road', 'Slippery road', 'Road narrows on the right', 'Road work', 'Traffic signals', 'Pedestrians', 'Children crossing', 'Bicycles crossing', 'Beware of ice/snow', 'Wild animals crossing', 'End of all speed and passing limits', 'Turn right ahead', 'Turn left ahead', 'Ahead only', 'Go straight or right', 'Go straight or left', 'Keep right', 'Keep left', 'Roundabout mandatory', 'End of no passing', 'End of no passing by vehicles over 3.5 metric tons']
-
-        print(
-        "This image most likely belongs to {} with a {:.2f} percent confidence."
-        .format(class_names[np.argmax(score)], 100 * np.max(score))
-        )
         
-        return predictions
+        return predictions , score
 
 def main():
     st.title("Traffic Sign Recognition")
@@ -40,13 +34,71 @@ def main():
         image = Image.open(file)
         st.image(image, use_column_width=True)
         predictions = import_and_predict(image, model)
-        score = tf.nn.softmax(predictions[0])
-        class_names=['Speed limit (20km/h)', 'Speed limit (30km/h)', 'Speed limit (50km/h)', 'Speed limit (60km/h)', 'Speed limit (70km/h)', 'Speed limit (80km/h)', 'End of speed limit (80km/h)', 'Speed limit (100km/h)', 'Speed limit (120km/h)', 'No passing', 'No passing for vehicles over 3.5 metric tons', 'Right-of-way at the next intersection', 'Priority road', 'Yield', 'Stop', 'No vehicles', 'Vehicles over 3.5 metric tons prohibited', 'No entry', 'General caution', 'Dangerous curve to the left', 'Dangerous curve to the right', 'Double curve', 'Bumpy road', 'Slippery road', 'Road narrows on the right', 'Road work', 'Traffic signals', 'Pedestrians', 'Children crossing', 'Bicycles crossing', 'Beware of ice/snow', 'Wild animals crossing', 'End of all speed and passing limits', 'Turn right ahead', 'Turn left ahead', 'Ahead only', 'Go straight or right', 'Go straight or left', 'Keep right', 'Keep left', 'Roundabout mandatory', 'End of no passing', 'End of no passing by vehicles over 3.5 metric tons']
+        class_names = [
+    "Speed limit (5km/h)",
+    "Speed limit (15km/h)",
+    "Speed limit (30km/h)",
+    "Speed limit (40km/h)",
+    "Speed limit (50km/h)",
+    "Speed limit (60km/h)",
+    "Speed limit (70km/h)",
+    "Speed limit (80km/h)",
+    "Don't Go straight or left",
+    "Don't Go straight or right",
+    "Don't Go straight",
+    "Don't Go left",
+    "Don't Go left or right",
+    "Don't Go right",
+    "Don't overtake from left",
+    "No U-turn",
+    "No Car",
+    "No horn",
+    "Speed limit (40km/h)",
+    "Speed limit (50km/h)",
+    "Go straight or right",
+    "Go straight",
+    "Go left",
+    "Go left or right",
+    "Go right",
+    "Keep left",
+    "Keep right",
+    "Roundabout mandatory",
+    "Watch out for cars",
+    "Horn",
+    "Bicycles crossing",
+    "U-turn",
+    "Road Divider",
+    "Traffic signals",
+    "Danger Ahead",
+    "Zebra Crossing",
+    "Bicycles crossing",
+    "Children crossing",
+    "Dangerous curve to the left",
+    "Dangerous curve to the right",
+    "Unknown1",
+    "Unknown2",
+    "Unknown3",
+    "Go right or straight",
+    "Go left or straight",
+    "Unknown4",
+    "ZigZag Curve",
+    "Train Crossing",
+    "Under Construction",
+    "Unknown5",
+    "Fences",
+    "Heavy Vehicle Accidents",
+    "Unknown6",
+    "Give Way",
+    "No stopping",
+    "No entry",
+    "Unknown7",
+    "Unknown8"
+]
+        st.write("This image most likely belongs to {} with a {:.2f} percent confidence."
+                 .format(class_names[np.argmax(predictions[0])], 100 * np.max(predictions[0])))
+        
+        
 
-        st.write(
-            "This image most likely belongs to {} with a {:.2f} percent confidence."
-            .format(class_names[np.argmax(score)], 100 * np.max(score))
-        )
 
 if __name__ == '__main__':
     main()
